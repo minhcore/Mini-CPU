30a9bfbc8ee6bdf913aabc08565f7b4cf0c7332d
minhcore <dpnhatminh11@gmail.com>
2025-09-06 17:00:37 +0700

    fixed terminal

--- terminal.py
+++ terminal.py
@@ -9,23 +9,32 @@ ser = serial.Serial(
 
 print("Wait for data...")
 last_data_time = time.time()
-INPUT_TRIGGER = 127
-RESET_MODE_TRIGGER = 128  
+
+INPUT_TRIGGER = 127       
+RESET_MODE_TRIGGER = -128 
 
 mode = None
+
+def to_signed8(val: int) -> int:
+    
+    return val if val < 128 else val - 256
+
+def to_unsigned8(val: int) -> int:
+    
+    return (val + 256) % 256
 
 while True:
     data = ser.read(1)
     if data:
         last_data_time = time.time()
-        byte_val = data[0]
+        signed_val = to_signed8(data[0])
 
-        if byte_val == RESET_MODE_TRIGGER:
+        if signed_val == RESET_MODE_TRIGGER:
             print("\n[CPU NOTICE] Please select new mode:")
             mode = input("Select mode (s=string, n=number): ").strip().lower()
             continue
 
-        if byte_val == INPUT_TRIGGER:
+        if signed_val == INPUT_TRIGGER:
             print("\n[CPU REQUEST] Please enter input: ")
             if mode is None:
                 mode = input("Select mode (s=string, n=number): ").strip().lower()
@@ -34,8 +43,8 @@ while True:
                 try:
                     num = int(input("Enter number (-128..127): "))
                     if -128 <= num <= 127:
-                        val = (num + 256) % 256
-                        ser.write(bytes([val]))
+                        ser.write(bytes([to_unsigned8(num)]))
+                        print(f"[HOST] Sent number: {num}")
                     else:
                         print("Out of range! Must be -128..127")
                 except ValueError:
@@ -47,7 +56,7 @@ while True:
             continue
 
         if mode == "n":
-            print(byte_val, end=" ", flush=True)
+            print(signed_val, end=" ", flush=True)
         else:
             try:
                 text = data.decode('ascii')
@@ -57,6 +66,6 @@ while True:
 
     else:
         if time.time() - last_data_time > 15:
-            print("No data for 15 seconds, maybe CPU halted. Closing serial...")
+            print("\nNo data for 15 seconds, maybe CPU halted. Closing serial...")
             ser.close()
             break
